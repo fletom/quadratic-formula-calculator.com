efef78a97834721ce08210a4e4106f5ba7dc9a81
Fletcher Tomalty <fletcher@tomalty.com>
2016-02-10 20:24:30 -0500

    improved deploy.sh removes the need for the src directory
    we can exclude .git now

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,15 @@
-S3_BUCKET_NAME='quadratic-formula-calculator.com'
+set -euf -o pipefail
 
-cd src
+S3_BUCKET_NAME='quadratic-formula-calculator.com'
+EXCLUDE=('.DS_Store' 'deploy.sh' '.git/*')
+
+exclude_params=''
+for e in ${EXCLUDE[@]}; do
+	exclude_params="$exclude_params --exclude $e"
+done
 
 aws s3 sync \
 	./ s3://$S3_BUCKET_NAME \
 	--acl 'public-read' \
+	$exclude_params \
 	--delete
